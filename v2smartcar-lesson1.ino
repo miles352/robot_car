d86f8b188b5d6ef84014557198c29e016c4d3153
miles352 <76878501+miles352@users.noreply.github.com>
2023-10-25 20:38:29 -0400

    Add files via upload

---
+++ v2smartcar-lesson1.ino
@@ -0,0 +1,127 @@
+/*  ___   ___  ___  _   _  ___   ___   ____ ___  ____  
+ * / _ \ /___)/ _ \| | | |/ _ \ / _ \ / ___) _ \|    \ 
+ *| |_| |___ | |_| | |_| | |_| | |_| ( (__| |_| | | | |
+ * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
+ *                  (____/ 
+ * Arduino Smart Car Tutorial Lesson 1
+ * Tutorial URL http://osoyoo.com/2018/12/07/new-arduino-smart-car-lesson1/
+ * CopyRight www.osoyoo.com
+
+ */
+#define speedPinR 9    //  RIGHT PWM pin connect MODEL-X ENA
+#define RightMotorDirPin1  12    //Right Forwards Motor direction pin 1 to MODEL-X IN1 
+#define RightMotorDirPin2  11    //Right Reverse Motor direction pin 2 to MODEL-X IN2
+#define speedPinL 6    // Left PWM pin connect MODEL-X ENB
+#define LeftMotorDirPin1  7    //Left Forwards Motor direction pin 1 to MODEL-X IN3 
+#define LeftMotorDirPin2  8   //Left Reverse Motor direction pin 1 to MODEL-X IN4 
+
+// this delay is calculated based on speed of 200
+// #define MSDelayPerCM 17.77777
+#define MSDelayPerCM 18.45
+
+/*motor control*/
+void forwards(int t = 0)  //Forward
+{
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+
+// void forwardsCM(byte speed, float cm)
+// {
+//   digitalWrite(RightMotorDirPin1, HIGH);
+//   digitalWrite(RightMotorDirPin2,LOW);
+//   digitalWrite(LeftMotorDirPin1,HIGH);
+//   digitalWrite(LeftMotorDirPin2,LOW);
+//   analogWrite(speedPinL,speed);
+//   analogWrite(speedPinR,speed);
+//   unsigned long ms = round(MSDelayPerCM * cm * (speed / 200.0));
+//   delay(ms);
+// }
+
+void forwardsCM(float cm)
+{
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(MSDelayPerCM * cm);
+}
+
+void turnLeft(int t = 0)  //Turn left
+{
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,HIGH);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+void turnRight(int t = 0)  //Turn right
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,HIGH);
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+void reverse(int t = 0)  //Reverse
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,HIGH);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,HIGH);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+void stop()    //Stop
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,LOW);
+}
+/*set motor speed */
+void setMotorSpeed(int speed_L,int speed_R)
+{
+  analogWrite(speedPinL,speed_L); 
+  analogWrite(speedPinR,speed_R);   
+}
+
+//Pins initialize
+void initGPIO()
+{
+	pinMode(RightMotorDirPin1, OUTPUT); 
+	pinMode(RightMotorDirPin2, OUTPUT); 
+	pinMode(speedPinL, OUTPUT);  
+ 
+	pinMode(LeftMotorDirPin1, OUTPUT);
+  pinMode(LeftMotorDirPin2, OUTPUT); 
+  pinMode(speedPinR, OUTPUT); 
+	stop();
+}
+
+
+
+void setup()
+{
+	initGPIO();
+	
+  forwardsCM(50);
+  
+  stop();
+
+}
+
+void loop(){
+}
