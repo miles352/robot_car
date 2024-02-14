fd46da6f32a64f552d4531e5fb4fa0f57619eefe
miles352 <76878501+miles352@users.noreply.github.com>
2024-02-14 17:05:51 -0500

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,18 +1,5 @@
 #include "Point.h"
 
-// Point::Point() : x(0), y(0) {};
-// Point::Point(int x, int y) : x(x), y(y) {};
-// Point::Point(const Point& other) : x(other.x), y(other.y) {};
-// bool Point::operator==(const Point& other) const
-// {
-//     return (x == other.x) && (y == other.y);
-// }
-// bool Point::operator<(const Point& other) const
-// {
-//     return (x < other.x) || ((x == other.x) && (y < other.y));
-// }
-
-
 byte x, y;
 bool Point::operator==(const Point& other) const
 {
