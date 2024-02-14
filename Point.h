4ebf0c1a916205787a9c51cea7f82636509ea4fb
miles352 <76878501+miles352@users.noreply.github.com>
2024-02-14 17:06:08 -0500

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,17 +1,6 @@
 #include "Arduino.h"
 #pragma once
 
-// struct Point 
-// {
-//     int x, y;
-//     bool operator==(const Point& other) const;
-//     bool operator<(const Point& other) const;
-//     Point();
-//     Point(int x, int y);
-//     Point(const Point& other);
-//     // ~Point();
-// };
-
 struct Point 
 {
     byte x, y;
